a824bc07de03991b462985c34df736a26dd26a37
mohitppatil2005-afk <mohitppatil2005@gmail.com>
2026-07-11 23:23:19 +0530

    Initial React project setup with routing

---
+++ frontend/src/router/AppRouter.jsx
@@ -0,0 +1,27 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import Search from "../pages/Search";
+import Bookmarks from "../pages/Bookmarks";
+import Profile from "../pages/Profile";
+import NotFound from "../pages/NotFound";
+
+function AppRouter(){
+    return(
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/login" element={<Login/>}/>
+                <Route path="/register" element={<Register/>}/>
+                <Route path="/search" element={<Search/>}/>
+                <Route path="/bookmarks" element={<Bookmarks/>}/>
+                <Route path="/profile" element={<Profile/>}/>
+                <Route path="*" element={<NotFound/>}/>
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default AppRouter;
