d22f64a7bbd7e53da6c7bd350d02c8ba0b6ad0b6
mohitppatil2005-afk <mohitppatil2005@gmail.com>
2026-07-13 22:58:13 +0530

    Integrated News API backend and improved frontend UI

---
+++ backend/app.py
@@ -0,0 +1,43 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+from dotenv import load_dotenv
+import requests
+import os
+
+load_dotenv()
+NEWS_API_KEY=os.getenv("NEWS_API_KEY")
+
+app= Flask(__name__)
+CORS(app)
+
+@app.route("/news")
+def get_news():
+    
+    url = "https://newsapi.org/v2/top-headlines"
+
+    category = request.args.get("category", "general")
+
+    params={
+        "country":"us",
+        "pageSize":20,
+        "category": category,
+        "apiKey":NEWS_API_KEY
+    }
+
+    response=requests.get(url, params=params)
+    data=response.json()
+    print(data)
+    articles=[]
+    for article in data.get("articles", []):
+        articles.append({
+            "title": article.get("title"),
+            "description": article.get("description"),
+            "image": article.get("urlToImage"),
+            "url": article.get("url"),
+            "source": article.get("source", {}).get("name"),
+            "publishedAt": article.get("publishedAt")
+        })
+    return jsonify(articles)
+
+if __name__=="__main__":
+    app.run(debug=True)
